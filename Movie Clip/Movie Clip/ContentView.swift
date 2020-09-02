74e1607a0a211f99c3ddcf160d824c2fc178e19c
Narcis <narcis.zait@gmail.com>
2020-09-02 19:18:52 +0200

    Initial commit

---
+++ Movie Clip/Movie Clip/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Movie Clip
+//
+//  Created by Narcis Zait on 02/09/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Movie Clip/Movie Clip/Movie_ClipApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_ClipApp.swift
+//  Movie Clip
+//
+//  Created by Narcis Zait on 02/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct Movie_ClipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
